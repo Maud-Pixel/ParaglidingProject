da925db4453137cefb39979f547cea0efbb33291
Alison Franck <al.franck@hotmail.com>
2020-02-16 15:43:07 +0100

    Ajout Model Position

---
+++ ParaglidingProject/Models/Position.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParaglidingProject.Models
+{
+    public class Position
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
