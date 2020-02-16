9424c79b59947007bcad9b9d639a5629f3995321
Maud-Pixel <60818074+Maud-Pixel@users.noreply.github.com>
2020-02-16 15:23:46 +0100

    Update LandingSite.cs
    J'ai supprimé et remis sous le même dossier que TakeOffSite (cf commentaires TakeOffSite)

--- ParaglidingProject/Models/LandingSite.cs
+++ ParaglidingProject/Models/LandingSite.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace ParaglidingProject.Models
-{
-    public class LandingSite
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Orientation { get; set; }
-        public string DescriptionAproach { get; set; }
-        public int LevelID { get; set; }
-        public ICollection<Flight> Flights { get; set; }
-        public Level RequiseLevel { get; set; }
-    }
-}
