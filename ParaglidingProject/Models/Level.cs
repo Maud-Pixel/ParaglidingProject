eb9021b491febb9721aad336770b7578ea27d51c
Maud-Pixel <60818074+Maud-Pixel@users.noreply.github.com>
2020-02-16 15:54:59 +0100

    Update Level.cs
    Numéro croissant à ajouter

--- ParaglidingProject/Models/Level.cs
+++ ParaglidingProject/Models/Level.cs
@@ -10,6 +10,7 @@ namespace ParaglidingProject.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public string Skill { get; set; }
+        public int DifficultyNumber {get; set;}
         public ICollection<License> Licenses { get; set; }
         public ICollection<LandingSite> LandingSite { get; set; }
     }
